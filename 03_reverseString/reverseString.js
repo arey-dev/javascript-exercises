5c9b84b76934370b7f9d0cf0e1817c8d418d884f
R.A Borja <raborja.dev@gmail.com>
2022-09-02 14:24:28 +0800

    03_REVERSESTRING done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+  if(string === '') return '';
 
+  let stringChars = [];
+  for(let char of string) {
+    stringChars.unshift(char);
+  }
+
+  return stringChars.join('');
 };
 
 // Do not edit below this line

ce11bbfba25950f80725099ba0f7e7db9ec757d9
R.A Borja <raborja.dev@gmail.com>
2022-09-02 15:28:49 +0800

    04_REMOVEFROMARRAY done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+  // get the array, which is the first argument
+  // get the rest and set it as array
+  // filter the array
+  // if element is equal to the element from the other arguments(array)
+  // do not return;
+  const [arr, ...otherArgs] = args;
 
+  return arr.filter(item => !otherArgs.includes(item));
 };
 
 // Do not edit below this line
